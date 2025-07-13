c145cfdc7215ba0a770ccbfb6d33de4c2e316f09
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-07-13 22:11:43 +0000

    refactor(`WrapPropertyBase`): add `controller` and `descriptor` getter, remove `target` in favor of `object`, handle `active` and `enabled`. Add ability to use custom controller.

--- src/lib/wrap-property-base.abstract.ts
+++ src/lib/wrap-property-base.abstract.ts
@@ -1,36 +1,75 @@
 // Abstract.
 import { WrapPropertyCore } from './wrap-property-core.abstract';
 // Interface.
-import { WrappedPropertyDescriptor } from '../interface';
+import { PropertyControllerShape } from '@typedly/controller';
+import { WrappedPropertyDescriptor } from '@typedly/descriptor';
 /**
  * @description The foundational class for wrapping properties.
  * @export
  * @abstract
  * @class WrapPropertyBase
- * @template {object | (new () => any)} T The type of the target object or class to wrap the property of the given `key`.
- * @template {Record<PropertyKey, any>} [O=(T extends new () => T ? ( T extends { prototype: infer P } ? P : never) : T)] The object type of captured `T`.
- * @template {keyof O extends string | symbol ? keyof O : never} [K=keyof O extends string | symbol ? keyof O : never] The key type of the property to wrap.
+ * @template {Record<PropertyKey, any>} O The type of the object.
+ * @template {keyof O} [K=keyof O] The key type of the property to wrap.
+ * @template {boolean} [A=boolean] The type of active property.
+ * @template {boolean} [F=boolean] The type of enabled property.
  * @template {boolean} [C=boolean] The type of configurable property.
  * @template {boolean} [E=boolean] The type of enumerable property.
  * @template {WrappedPropertyDescriptor<O, K, C, E>} [D=WrappedPropertyDescriptor<O, K, C, E>] The type of descriptor constrained by the `WrappedPropertyDescriptor`.
- * @extends {WrapPropertyCore<T, O, K, C, E, D>}
+ * @template {PropertyControllerShape<O, K, A, C, E, D>} [R=PropertyControllerShape<O, K, A, C, E, D>] The type of controller that controls the wrapping behavior.
+ * @extends {WrapPropertyCore<T, O, K, C, E, D, R>}
  */
 export abstract class WrapPropertyBase<
-  T extends object | (new () => any),
-  O extends Record<PropertyKey, any> = (T extends new () => T ? (T extends { prototype: infer P } ? P : never) : T),
-  K extends keyof O extends string | symbol ? keyof O : never = keyof O extends string | symbol ? keyof O : never,
+  O extends Record<PropertyKey, any>,
+  K extends keyof O = keyof O,
+  A extends boolean = boolean,
+  F extends boolean = boolean,
   C extends boolean = boolean,
   E extends boolean = boolean,
-  D extends WrappedPropertyDescriptor<O, K, C, E> = WrappedPropertyDescriptor<O, K, C, E>,
-> extends WrapPropertyCore<T, O, K, C, E, D> {
+  D extends WrappedPropertyDescriptor<O, K, A, F, C, E> = WrappedPropertyDescriptor<O, K, A, F, C, E>,
+  R extends PropertyControllerShape<O, K, A, F, C, E, D> = PropertyControllerShape<O, K, A, F, C, E, D>
+> extends WrapPropertyCore<O, K, A, F, C, E, D, R> {
+  /**
+   * @inheritdoc
+   * @public
+   * @readonly
+   * @type {R}
+   */
+  public override get controller() {
+    return this.#controller as R;
+  }
+
+  /**
+   * @description The descriptor that handles actual wrapped property.
+   * @public
+   * @readonly
+   * @type {D}
+   */
+  public get descriptor(): D {
+    return this.controller
+      ? this.controller.descriptor
+      : this.#descriptor as D;
+  }
+
   /**
    * @description The key of the property to wrap.
    * @protected
    * @readonly
    * @type {K}
    */
-  protected get key() {
+  protected get key(): K {
     return this.#key;
+  }
+
+  /**
+   * @description The previous descriptor of the wrapped property.
+   * @protected
+   * @readonly
+   * @type {*}
+   */
+  protected get previousDescriptor(): WrappedPropertyDescriptor<O, K, A, C, E> | PropertyDescriptor | undefined {
+    return this.controller
+      ? this.controller.descriptor.previousDescriptor
+      : this.descriptor.previousDescriptor;
   }
 
   /**
@@ -39,8 +78,10 @@ export abstract class WrapPropertyBase<
    * @readonly
    * @type {PropertyKey}
    */
-  protected get privateKey() {
-    return this.#privateKey;
+  protected get privateKey(): PropertyKey {
+    return (this.controller
+      ? this.controller.descriptor.privateKey
+      : this.descriptor.privateKey) as PropertyKey;
   }
 
   /**
@@ -49,9 +90,21 @@ export abstract class WrapPropertyBase<
    * @readonly
    * @type {T}
    */
-  protected get target() {
-    return this.#target;
+  protected get object(): O {
+    return this.#object;
   }
+
+  /**
+   * @description Privately stored controller that controls the wrapping behavior.
+   * @type {?R}
+   */
+  #controller?: R;
+
+  /**
+   * @description
+   * @type {D}
+   */
+  #descriptor?: D;
 
   /**
    * @description Privately stored key of property.
@@ -60,59 +113,52 @@ export abstract class WrapPropertyBase<
   #key: K;
 
   /**
-   * @description Previous descriptor to unwrap
-   * @type {?PropertyDescriptor}
-   */
-  #previousDescriptor?: PropertyDescriptor;
-
-  /**
-   * @description The private key under which the property value is stored.
-   * @type {PropertyKey}
-   */
-  #privateKey: PropertyKey;
-
-  /**
    * @description The target in which the property is wrapped.
-   * @type {T}
+   * @type {O}
    */
-  #target: T;
+  #object: O;
 
   /**
    * Creates an instance of `WrapPropertyBase` child class.
    * @constructor
-   * @param {T} target The target object or class to wrap the property of the given `key`.
+   * @param {O} object The target object or class to wrap the property of the given `key`.
    * @param {K} key The key to wrap the property.
    * @param {?D} [descriptor] The descriptor of the property to wrap.
+   * @param {?new (descriptor?: D) => R} [controller] The controller that controls the wrapping behavior.
    */
   constructor(
-    target: T,
+    object: O,
     key: K,
     descriptor?: D,
+    controller?: new (descriptor?: D) => R
   ) {
     super();
-    const object = (typeof target === 'function' ? target.prototype : target) as O;
 
-    // Assign the key, target, and private key.
+    // If the descriptor is not provided, create a default one.
+    descriptor = {
+      ...{
+        active: WrapPropertyCore.active,
+        configurable: WrapPropertyCore.configurable,
+        enumerable: WrapPropertyCore.enumerable,
+        previousDescriptor: Object.getOwnPropertyDescriptor(object, key) as PropertyDescriptor,
+        privateKey: `_${String(key)}`
+      },
+      ...descriptor,
+    } as D;
+
+    // If the controller is provided, create an instance of it.
+    controller
+      ? this.#controller = new controller(descriptor)
+      : this.#descriptor = descriptor;
+
+    // The key to wrap.
     this.#key = key;
-    this.#target = target;
-    this.#privateKey = descriptor?.privateKey || `_${String(key)}`; // Set the private key if not provided.
+  
+    // The target object to wrap the property on.
+    this.#object = object;
 
     // Define the private property to store the value.
     this.#hasPrivateProperty(object) === false && this.#definePrivateProperty(object, key);
-  }
-
-  /**
-   * @description Gets the previous descriptor of the property that is used before wrapping property.
-   * @protected
-   * @param {O} object The object to get the previous descriptor from.
-   * @param {K} key The key of the property to get the previous descriptor.
-   * @returns {(PropertyDescriptor | undefined)} 
-   */
-  protected getPreviousDescriptor(object: O, key: K): PropertyDescriptor | undefined {
-    this.#previousDescriptor = !this.#previousDescriptor
-      ? Object.getOwnPropertyDescriptor(object, key) as PropertyDescriptor
-      : this.#previousDescriptor as PropertyDescriptor;
-    return this.#previousDescriptor;
   }
 
   /**
@@ -121,85 +167,96 @@ export abstract class WrapPropertyBase<
    * @returns {this} 
    */
   public unwrap(): this {
-    Object.defineProperty((typeof this.#target === 'function' ? this.#target.prototype : this.#target) as O, this.#key, this.#previousDescriptor!);
-    delete (typeof this.target === 'function' ? this.target.prototype : this.#target)[this.#privateKey];
+    const previousDescriptor = ((this.#controller ? this.#controller.descriptor : this.#descriptor) as D).previousDescriptor;
+    previousDescriptor &&
+      (
+        Object.defineProperty(this.object, this.key, previousDescriptor),
+        this.privateKey && delete this.object[this.privateKey]
+      );
     return this;
   }
-  
+
   /**
    * @description Wraps the property with a private indicator.
    * @protected
    * @param {O} object The object to wrap the property on.
    * @param {K} key The key of the property to wrap.
-   * @param {WrappedPropertyDescriptor<O, K>} [param0={}] The options for wrapping the property.
-   * @param {WrappedPropertyDescriptor<O, K>} param0.configurable Whether the property is configurable.
-   * @param {WrappedPropertyDescriptor<O, K>} param0.enumerable Whether the property is enumerable.
-   * @param {WrappedPropertyDescriptor<O, K>} param0.onGet The function to call when getting the property.
-   * @param {WrappedPropertyDescriptor<O, K>} param0.onSet The function to call when setting the property.
-   * @param {WrappedPropertyDescriptor<O, K>} param0.privateKey The private key to use for the property.
    * @returns {this}
    */
   protected wrap(
     object: O,
-    key: K, {
-      configurable,
-      enumerable,
-      onGet,
-      onSet,
-      privateKey,
-    }: WrappedPropertyDescriptor<O, K> = {},
+    key: K
   ): this {
-    // Get the previous descriptor of the property.
-    const previousDescriptor = this.getPreviousDescriptor(object, key);
+    // Wrap acts as controller.
+    const controller = this.#controller ? this.#controller : this;
+
+    // The getter for the property.
+    const get = controller.descriptor.get
+      ? controller.descriptor.get
+      : function(this: O): O[K] {
+        if (controller.descriptor.enabled === false) {
+          return undefined as O[K];
+        }
+
+        // Set the this as the target object.
+        const o = (this as O);
+
+        // Get the previous value from descriptor.
+        const previousValue = controller.descriptor.previousDescriptor
+          ? controller.descriptor.previousDescriptor.get && typeof controller.descriptor.previousDescriptor.get === 'function'
+            ? controller.descriptor.previousDescriptor.get.call(this)
+            : (controller.descriptor.previousDescriptor as PropertyDescriptor).value
+          : undefined;
+
+
+        // Current descriptor.
+        return controller.descriptor.onGet
+          && ((typeof controller.descriptor.active === 'boolean' && controller.descriptor.active)
+            || (typeof controller.descriptor.active === 'object' && controller.descriptor.active.onGet === true))
+          ? controller.descriptor.onGet.call(o, key, previousValue, o[controller.descriptor.privateKey as K] as O[K], o) as O[K]
+          : o[controller.descriptor.privateKey as K] as O[K];
+      };
+
+    // The setter for the property.
+    const set = controller.descriptor.set
+      ? controller.descriptor.set
+      : function(this: O, value: O[K]): void {
+        if (controller.descriptor.enabled === false) {
+          return undefined as O[K];
+        }
+
+        // Set the this as the target object.
+        const o = (this as O);
+
+        // Get the previous value from previous descriptor or current value.
+        const previousValue = (controller.descriptor.privateKey as keyof O || (controller.descriptor.previousDescriptor as PropertyDescriptor)?.value) as O[K];
+
+        // Perform previous descriptor.
+        controller.descriptor.previousDescriptor?.set && controller.descriptor.previousDescriptor.set.call(o, value);
+
+        // Set the private property value.
+        Object.assign(
+          o,
+          {
+            [controller.descriptor.privateKey as K]: controller.descriptor.onSet
+              && (
+                (typeof controller.descriptor.active === 'boolean' && controller.descriptor.active)
+                || (typeof controller.descriptor.active === 'object' && controller.descriptor.active.onSet === true)
+              )
+              ? controller.descriptor.onSet.call(o, value, previousValue, key, o) as O[K]
+              : value
+          }
+        );
+      };
 
     // Define property with the given key and options.
     Object.defineProperty(
       object,
       key, {
-        // Whether the property can be deleted or changed.
-        configurable: configurable === undefined ? WrapPropertyCore.configurable : configurable,
-
-        // Whether the property is visible in enumerations.
-        enumerable: enumerable === undefined ? WrapPropertyCore.enumerable : enumerable,
-
-        // Getter for the property.
-        ...{
-          get(): O[K] { 
-            // Set the this as the target object.
-            const t = (this as O);
-
-            // Get the previous value from descriptor.
-            const previousValue = previousDescriptor
-              ? previousDescriptor.get && typeof previousDescriptor.get === 'function'
-                ? previousDescriptor.get.call(this)
-                : previousDescriptor.value
-              : undefined;
-
-            // Current descriptor.
-            return onGet && typeof onGet === 'function'
-              ? onGet.call(t, key, t[privateKey as keyof O] as O[K], previousValue, t) as O[K]
-              : t[privateKey as keyof O] as O[K];
-          }
-        },
-
-        // Setter for the property.
-        ...{
-          set(value: O[K]): void {
-            // Set the this as the target object.
-            const t = (this as O);
-
-            // Get the previous value from previous descriptor or current value.
-            const previousValue = (t[privateKey as keyof O] || (previousDescriptor as PropertyDescriptor)?.value) as O[K];
-
-            // Perform previous descriptor.
-            previousDescriptor?.set && previousDescriptor.set.call(t, value);
-
-            // Set the private property value.
-            t[privateKey as keyof O] = onSet && typeof onSet === 'function'
-              ? onSet.call(t, value, previousValue, key, t) as O[K]
-              : value;
-          }
-        },
+        configurable: controller.descriptor.configurable, // Whether the property can be deleted or changed.
+        enumerable: controller.descriptor.enumerable, // Whether the property is visible in enumerations.
+        get, // Getter for the property.
+        set, // Setter for the property.
       }
     );
     return this;
