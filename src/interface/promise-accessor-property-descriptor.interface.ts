9f0e2e22509c231f6cd80751bf2418e8e170513f
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-07-13 22:12:05 +0000

    refactor(interface): remove.

--- src/interface/promise-accessor-property-descriptor.interface.ts
+++
@@ -1,6 +0,0 @@
-export type PromiseAccessorPropertyDescriptor<T, K extends keyof T> = {
-  get?: (this: T) => Promise<T[K]>;
-  set?: (this: T, value: T[K] | Promise<T[K]>) => void;
-  configurable?: boolean;
-  enumerable?: boolean;
-};
