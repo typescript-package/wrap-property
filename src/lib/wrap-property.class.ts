bda0dc74c36925df81da4029446e5047b2fc7c12
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-07-14 03:13:02 +0000

    refactor(`WrapProperty`): add `controller`,  and simplify removing the target.

--- src/lib/wrap-property.class.ts
+++ src/lib/wrap-property.class.ts
@@ -1,7 +1,8 @@
 // Abstract.
 import { WrapPropertyBase } from './wrap-property-base.abstract';
 // Interface.
-import { WrappedPropertyDescriptor } from '../interface';
+import { PropertyControllerShape } from '@typedly/controller';
+import { WrappedPropertyDescriptor } from '@typedly/descriptor';
 /**
  * @description The concrete class for wrapping object properties.
  * @export
@@ -9,43 +10,49 @@ import { WrappedPropertyDescriptor } from '../interface';
  * @template {object | (new () => any)} T The type of the target object or class to wrap the property of the given `key`.
  * @template {Record<PropertyKey, any>} [O=(T extends new () => T ? ( T extends { prototype: infer P } ? P : never) : T)] The object type of captured `T`.
  * @template {keyof O extends string | symbol ? keyof O : never} [K=keyof O extends string | symbol ? keyof O : never] The key type of the property to wrap.
+ * @template {boolean} [A=boolean] The type of active property.
  * @template {boolean} [C=boolean] The type of configurable property.
  * @template {boolean} [E=boolean] The type of enumerable property.
  * @template {WrappedPropertyDescriptor<O, K, C, E>} [D=WrappedPropertyDescriptor<O, K, C, E>] The type of descriptor constrained by the `WrappedPropertyDescriptor`.
+ * @template {PropertyControllerShape<O, K, A, C, E, D>} [Controller=PropertyControllerShape<O, K, A, C, E, D>] The type of controller that controls the wrapping behavior.
  * @extends {WrapPropertyBase<T, O, K, C, E, D>}
  */
 export class WrapProperty<
   // Used to determine the type of the target object for picking the key from prototype.
-  T extends object | (new () => any),
-  O extends Record<PropertyKey, any> = (T extends new () => T ? ( T extends { prototype: infer P } ? P : never) : T),
+  O extends Record<PropertyKey, any>,
   K extends keyof O extends string | symbol ? keyof O : never = keyof O extends string | symbol ? keyof O : never,
+  A extends boolean = boolean,
+  F extends boolean = boolean,
   C extends boolean = boolean,
   E extends boolean = boolean,
-  D extends WrappedPropertyDescriptor<O, K, C, E> = WrappedPropertyDescriptor<O, K, C, E>,
-> extends WrapPropertyBase<T, O, K, C, E, D> {
+  D extends WrappedPropertyDescriptor<O, K, A, F, C, E> = WrappedPropertyDescriptor<O, K, A, F, C, E>,
+  Controller extends PropertyControllerShape<O, K, A , F, C, E, D> = PropertyControllerShape<O, K, A, F, C, E, D>
+> extends WrapPropertyBase<O, K, A, F, C, E, D> {
   /**
    * Creates an instance of `WrapProperty`.
    * @constructor
-   * @param {T} target The target object or class to wrap the property of the given `key`.
+   * @param {O} object The target object or class to wrap the property of the given `key`.
    * @param {K} key The key of the property to wrap.
    * @param {?D} [descriptor] The descriptor of the property to wrap.
+   * @param {?new (descriptor?: D) => Controller} [controller] The controller that controls the wrapping behavior.
    */
   constructor(
-    target: T,
+    object: O,
     key: K,
-    descriptor?: D
+    descriptor?: D,
+    controller?: new (descriptor?: D) => Controller
   ) {
     super(
-      target,
+      object,
       key,
-      descriptor
+      descriptor,
+      controller
     );
 
     // Define the property with the given key and options.
     this.wrap(
-      (typeof target === 'function' ? target.prototype : target) as O,
-      key,
-      descriptor
+      object,
+      key
     );
   }
 }
